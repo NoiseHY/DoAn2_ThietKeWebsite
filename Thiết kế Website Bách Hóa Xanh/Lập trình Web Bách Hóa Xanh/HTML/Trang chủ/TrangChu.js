d0633e25d0f8e3deaf0b4c20e50844bddfe794f1
NoiseHY <congnamhy1@gmail.com>
2023-11-29 08:41:42 +0700

    Trang chủ hoàn thiện 90 %

--- Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang chủ/TrangChu.js
+++ Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang chủ/TrangChu.js
@@ -1,31 +1,24 @@
-//Thêm hiệu ứng vào button trái và phải của slide
 let slideIndex = 1;
 showSlides(slideIndex);
 
+// Tự động chạy slide sau mỗi 7 giây
+setInterval(function() { 
+  plusSlides(1);
+}, 7000);
+
+// Hàm thay đổi slide
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
+// Hàm hiển thị slide
 function showSlides(n) {
   let i;
-  const slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";
 }
-
-// Tự động chuyển slide mỗi 7 giây
-setInterval(function() {
-  plusSlides(1);
-}, 7000);
