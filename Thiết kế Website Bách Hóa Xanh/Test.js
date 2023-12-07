9b8820a68a67a5be82a543c55e6b78909ef68c74
NoiseHY <congnamhy1@gmail.com>
2023-12-07 14:43:41 +0700

    thêm thư viện angular JS thành công

--- Thiết kế Website Bách Hóa Xanh/Test.js
+++ Thiết kế Website Bách Hóa Xanh/Test.js
@@ -1,8 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const chartMonths = document.querySelectorAll(".chart-month");
-  chartMonths.forEach(month => {
-    const value = month.querySelector(".bar").getAttribute("data-value");
-    const bar = month.querySelector(".bar");
-    bar.style.height = value + "px";
-  });
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.courses = ["Math", "Science", "History", "English"];
+    $scope.message = "Hello, AngularJS!";
 });
