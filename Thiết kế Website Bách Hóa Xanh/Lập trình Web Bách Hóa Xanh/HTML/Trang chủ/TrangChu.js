2578cd64d2f16be30dc4116ec6ed9e5d2aae8aa7
NoiseHY <congnamhy1@gmail.com>
2023-12-22 15:31:07 +0700

    Update giao diện trang chủ

--- Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang chủ/TrangChu.js
+++ Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang chủ/TrangChu.js
@@ -2,7 +2,7 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 // Tự động chạy slide sau mỗi 7 giây
-setInterval(function() { 
+setInterval(function () {
   plusSlides(1);
 }, 7000);
 
@@ -15,18 +15,16 @@ function plusSlides(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
 var imgDanhMuc = document.getElementById('imgDanhMuc');
 
-
-
-imgDanhMuc.addEventListener('click', function(){
-  window.location.href = 'http://127.0.0.1:5500/L%E1%BA%ADp%20tr%C3%ACnh%20Web%20B%C3%A1ch%20H%C3%B3a%20Xanh/HTML/Chi%20ti%E1%BA%BFt%20s%E1%BA%A3n%20ph%E1%BA%A9m/Chi%20ti%E1%BA%BFt%20s%E1%BA%A3n%20ph%E1%BA%A9m.html';
+imgDanhMuc.addEventListener('click', function () {
+  window.location.href = 'http://127.0.0.1:5500/L%E1%BA%ADp%20tr%C3%ACnh%20Web%20B%C3%A1ch%20H%C3%B3a%20Xanh/HTML/Trang%20danh%20m%E1%BB%A5c/Danhmuc.html';
 })
