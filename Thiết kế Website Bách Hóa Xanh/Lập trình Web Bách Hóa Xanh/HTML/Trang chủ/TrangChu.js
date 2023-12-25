16bb606cb9919af87f28566b97a553eac67764da
NoiseHY <congnamhy1@gmail.com>
2023-12-25 11:15:41 +0700

    Thêm local storage cho trang trang chủ và giỏ hàng
    local storage, tính toán cho trang giỏ hàng

--- Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang chủ/TrangChu.js
+++ Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang chủ/TrangChu.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Các đoạn mã để đếm ngược
   const phầnTửNgày = document.querySelector("#days");
   const phầnTửGiờ = document.querySelector("#hours");
@@ -39,130 +39,69 @@ document.addEventListener('DOMContentLoaded', function() {
     window.location.href = 'http://127.0.0.1:5500/L%E1%BA%ADp%20tr%C3%ACnh%20Web%20B%C3%A1ch%20H%C3%B3a%20Xanh/HTML/Trang%20danh%20m%E1%BB%A5c/Danhmuc.html';
   });
 
-  // Event listener cho hình ảnh
-  var imgCart = document.getElementById('imgCart');
-  imgCart.addEventListener('click', function () {
-    window.location.href = 'http://127.0.0.1:5500/L%E1%BA%ADp%20tr%C3%ACnh%20Web%20B%C3%A1ch%20H%C3%B3a%20Xanh/HTML/Trang%20gi%E1%BB%8F%20h%C3%A0ng/Gi%E1%BB%8F%20h%C3%A0ng.html';
-  });
+  // // Event listener cho hình ảnh
+  // var imgCart = document.getElementById('imgCart');
+  // imgCart.addEventListener('click', function () {
+  //   window.location.href = 'http://127.0.0.1:5500/L%E1%BA%ADp%20tr%C3%ACnh%20Web%20B%C3%A1ch%20H%C3%B3a%20Xanh/HTML/Trang%20gi%E1%BB%8F%20h%C3%A0ng/Gi%E1%BB%8F%20h%C3%A0ng.html';
+  // });
 
 
-  document.addEventListener('DOMContentLoaded', function() {
-    // Các đoạn mã để đếm ngược
-    const phầnTửNgày = document.querySelector("#days");
-    const phầnTửGiờ = document.querySelector("#hours");
-    const phầnTửPhút = document.querySelector("#minutes");
-    const phầnTửGiây = document.querySelector("#seconds");
-  
-    const thờiGianMụcTiêu = new Date("2023-12-27T00:00:00").getTime();
-  
-    function cậpNhậtĐồngHồ() {
-      const thờiGianHiệnTại = new Date().getTime();
-      const khoảngCáchThờiGian = thờiGianMụcTiêu - thờiGianHiệnTại;
-  
-      if (khoảngCáchThờiGian > 0) {
-        const sốNgàyCònLại = Math.floor(khoảngCáchThờiGian / (1000 * 60 * 60 * 24));
-        const sốGiờCònLại = Math.floor((khoảngCáchThờiGian % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const sốPhútCònLại = Math.floor((khoảngCáchThờiGian % (1000 * 60 * 60)) / (1000 * 60));
-        const sốGiâyCònLại = Math.floor((khoảngCáchThờiGian % (1000 * 60)) / 1000);
-  
-        phầnTửNgày.textContent = sốNgàyCònLại;
-        phầnTửGiờ.textContent = sốGiờCònLại.toString().padStart(2, '0');
-        phầnTửPhút.textContent = sốPhútCònLại.toString().padStart(2, '0');
-        phầnTửGiây.textContent = sốGiâyCònLại.toString().padStart(2, '0');
-      } else {
-        phầnTửNgày.textContent = '00';
-        phầnTửGiờ.textContent = '00';
-        phầnTửPhút.textContent = '00';
-        phầnTửGiây.textContent = '00';
-        clearInterval(cậpNhậtMỗiGiây);
-      }
-    }
-  
-    const cậpNhậtMỗiGiây = setInterval(cậpNhậtĐồngHồ, 1000);
-    cậpNhậtĐồngHồ(); 
-    
-  });
-  
-  let slideIndex = 1;
-    showSlides(slideIndex);
-  
-    // Tự động chạy slide sau mỗi 7 giây
-    setInterval(function () {
-      plusSlides(1);
-    }, 7000);
-  
-    // Hàm thay đổi slide
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-  
-    // Hàm hiển thị slide
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("slide");
-      if (n > slides.length) { slideIndex = 1 }
-      if (n < 1) { slideIndex = slides.length }
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slides[slideIndex - 1].style.display = "block";
-    }
-      
-});
+}); 
 
 
 
 let slideIndex = 1;
-  showSlides(slideIndex);
+showSlides(slideIndex);
 
-  // Tự động chạy slide sau mỗi 7 giây
-  setInterval(function () {
-    plusSlides(1);
-  }, 7000);
+// Tự động chạy slide sau mỗi 7 giây
+setInterval(function () {
+  plusSlides(1);
+}, 7000);
 
-  // Hàm thay đổi slide
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+// Hàm thay đổi slide
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Hàm hiển thị slide
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  slides[slideIndex - 1].style.display = "block";
+}
 
-  // Hàm hiển thị slide
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-  }
+let productList = [];
 
-  function addToLocalStorage() {
-    const productImg = document.getElementById('product-img').src;
-    const productName = document.getElementById('product-name').textContent;
-    const productPrice = document.getElementById('product-price').textContent;
-    const productSales = document.getElementById('product-sales').textContent;
-    const productStars = document.getElementById('product-stars').textContent;
-    const productPt = document.getElementById('product-pt').textContent;
 
-    const productData = {
-      img: productImg,
-      name: productName,
-      price: productPrice,
-      sales: productSales,
-      stars : productStars,
-      pt : productPt
-    };
-  
-    const existingProductData = JSON.parse(localStorage.getItem('selectedProduct'));
-  
-    if (existingProductData) {
-      // Xóa dữ liệu sản phẩm cũ nếu đã tồn tại
-      localStorage.removeItem('selectedProduct');
-      console.log('Dữ liệu sản phẩm cũ đã được xóa khỏi Local Storage');
-    }
-  
-    localStorage.setItem('selectedProduct', JSON.stringify(productData));
-    console.log('Dữ liệu sản phẩm mới đã được lưu vào Local Storage');
-  }
-  
+function addToProductList(element) {
+  const product = {};
+
+  // Lấy các thông tin sản phẩm từ phần tử HTML
+  const parent = element.closest('.Sanpham-tpye');
+  product.img = parent.querySelector('#product-img').src;
+  product.name = parent.querySelector('#product-name').textContent;
+  product.price = parent.querySelector('#product-price').textContent;
+  product.sales = parent.querySelector('#product-sales').textContent;
+  product.star = parent.querySelector('#product-stars').textContent;
+  product.pt = parent.querySelector('#product-pt').textContent;
+
+  // Thêm sản phẩm vào danh sách
+  productList.push(product);
+
+  console.log('Sản phẩm đã được thêm vào danh sách:', productList);
+}
+
+function convertToJSON() {
+  const productListJSON = JSON.stringify(productList);
+  localStorage.setItem('productListJSON', productListJSON);
+
+  // Đợi 
+  setTimeout(() => {
+    window.location.href = 'http://127.0.0.1:5500/L%E1%BA%ADp%20tr%C3%ACnh%20Web%20B%C3%A1ch%20H%C3%B3a%20Xanh/HTML/Trang%20gi%E1%BB%8F%20h%C3%A0ng/Gi%E1%BB%8F%20h%C3%A0ng.html';
+  }, 100);
+}
--- Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang giỏ hàng/GIỏ hàng.js
+++ Thiết kế Website Bách Hóa Xanh/Lập trình Web Bách Hóa Xanh/HTML/Trang giỏ hàng/GIỏ hàng.js
@@ -1,23 +1,88 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Lấy dữ liệu từ local storage
+  const productListFromLocalStorage = localStorage.getItem('productListJSON');
+  const productList = JSON.parse(productListFromLocalStorage) || [];
 
-  let number = 0;
-  const numberElement = document.querySelector('.ipNumbers');
+  function renderProducts() {
+    const cartContainer = document.querySelector('.cart-container-info');
 
-  const increaseButton = document.querySelector('.btTang');
-  if (increaseButton) {
-    increaseButton.addEventListener('click', () => {
-      number++;
-      numberElement.textContent = number;
+    productList.forEach(product => {
+      const productDiv = document.createElement('div');
+      productDiv.classList.add('cart-container-product');
+
+      productDiv.innerHTML = `
+        <div class="cart-container-product">
+          <div class="cart-container-product-img">
+            <img src="${product.img}">
+          </div>
+          <div class="cart-container-product-info">
+            <p>${product.name.trim()}</p>
+            <div class="cart-container-product-info-container">
+              <div class="sales-txt">
+                <p style="text-decoration-line: line-through; color: black;" id="product-price">
+                  ${product.price.trim()}
+                </p>
+                <p id="product-sales">
+                  ${product.sales.trim()}
+                </p>
+              </div>
+              <div class="sales-stars">
+                <div class="cart-container-product-info-sales">
+                  <a class="product-stars">
+                    ${product.star.trim()} <img src="/Lập trình Web Bách Hóa Xanh/HTML/Trang chủ/Anh/SanPham/star.png">
+                  </a>
+                  <p>
+                    ${product.pt.trim()}
+                  </p>
+                </div>
+              </div>                           
+            </div>
+            <div class="cart-container-product-info-bt">
+              <button>
+                Xóa sản phẩm 
+              </button>
+              <a>
+                Sản phẩm tương tự &#62; &#62;
+              </a>
+            </div>
+          </div>
+        </div>
+      `;
+
+      cartContainer.appendChild(productDiv);
     });
   }
 
-  const decreaseButton = document.querySelector('.btGiam');
-  if (decreaseButton) {
-    decreaseButton.addEventListener('click', () => {
-      if (number > 0) {
-        number--;
-        numberElement.textContent = number;
-      }
+  // Gọi hàm render 
+  renderProducts();
+
+  // Tính tổng tiền
+  function calculateTotalPrice() {
+    const totalPriceElement = document.querySelector('.cart-price-tt p');
+    let total = 0;
+
+    productList.forEach(product => {
+      const price = parseFloat(product.sales.trim().replace('đ', '').replace('.', '').replace(',', '.'));
+      total += price;
     });
+
+    totalPriceElement.textContent = `${total.toFixed(2).replace('.', ',')}đ`;
   }
+
+  // Tính phí vận chuyển và tổng thanh toán
+  function calculateShippingAndTotal() {
+    const shippingFeeElement = document.querySelector('.cart-price-vc p');
+    const totalPaymentElement = document.querySelector('.cart-price-tong p');
+    const shippingFee = 50000; // Giả sử phí vận chuyển là 50.000đ
+
+    shippingFeeElement.textContent = `${shippingFee.toFixed(2).replace('.', ',')}đ`;
+
+    const totalPrice = parseFloat(document.querySelector('.cart-price-tt p').textContent.replace('đ', '').replace('.', '').replace(',', '.'));
+    const totalPayment = totalPrice + shippingFee;
+    totalPaymentElement.textContent = `${totalPayment.toFixed(2).replace('.', ',')}đ`;
+  }
+
+  // Gọi hàm tính tổng tiền, phí vận chuyển và tổng thanh toán
+  calculateTotalPrice();
+  calculateShippingAndTotal();
 });
